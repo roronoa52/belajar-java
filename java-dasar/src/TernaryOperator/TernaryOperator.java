1fd60f3122be1a70f087553f7670e0ad99a3fb37
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-18 13:40:57 +0700

    learn ternary operator

---
+++ java-dasar/src/TernaryOperator/TernaryOperator.java
@@ -0,0 +1,12 @@
+package TernaryOperator;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        var nilai = 10;
+        String ucapan = nilai >= 50 ? "Selamat" : "selmuy";
+
+        System.out.println(ucapan);
+
+    }
+}
