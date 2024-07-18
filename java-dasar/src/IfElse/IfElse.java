1b638624f486c514cc81749c42ed9e5d25af6b26
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-18 13:31:01 +0700

    learn if,if else, else if

---
+++ java-dasar/src/IfElse/IfElse.java
@@ -0,0 +1,17 @@
+package IfElse;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        var nilai = 101;
+
+        if(nilai <= 50){
+            System.out.println("kamu gagal");
+        } else if (nilai <= 100) {
+            System.out.println("kamu lulus");
+        }else {
+        System.out.println("kamu mengilang");
+        }
+
+    }
+}
