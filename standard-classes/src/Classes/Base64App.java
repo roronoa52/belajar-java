965b4b803330f1d826dfda8966806b157a81924e
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-19 02:00:12 +0700

    learn Objects

---
+++ standard-classes/src/Classes/Base64App.java
@@ -0,0 +1,17 @@
+package Classes;
+
+import java.util.Base64;
+
+public class Base64App {
+    public static void main(String[] args) {
+        String original = "Farhan Yudha Pratama";
+
+        String encoded = Base64.getEncoder().encodeToString(original.getBytes());
+        System.out.println(encoded);
+
+        byte[] bytes = Base64.getDecoder().decode(encoded);
+        String result = new String(bytes);
+
+        System.out.println(result);
+    }
+}
--- standard-classes/src/Classes/UUIDApp.java
+++ standard-classes/src/Classes/UUIDApp.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class UUIDApp {
     public static void main(String[] args) {
         for(var i = 0; i < 100; i++){
-            
+
             UUID uuid = UUID.randomUUID();
             String key = uuid.toString();
 
