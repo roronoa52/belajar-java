6c0701d99ac47a667fe2048fdf9d9f64e94e9049
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-19 17:45:49 +0700

    Learn Ordering Operation

---
+++ belajar-stream/src/test/java/belajar/stream/java/OrderingOperationTest.java
@@ -0,0 +1,26 @@
+package belajar.stream.java;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class OrderingOperationTest {
+
+    @Test
+    void testSorted() {
+        List.of("Eko", "Kurniawan", "Khannedy", "Programmer", "Zaman", "Now").stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    @Test
+    void testSortedWithComparator() {
+        Comparator<String> reverseComparator = Comparator.reverseOrder();
+
+        List.of("Eko", "Kurniawan", "Khannedy", "Programmer", "Zaman", "Now").stream()
+                .sorted(reverseComparator)
+                .forEach(System.out::println);
+    }
+
+}
