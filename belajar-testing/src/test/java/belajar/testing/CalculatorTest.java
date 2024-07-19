500f204c5ec323dd1187b1562890d3865bcbf165
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-19 10:50:35 +0700

    Change name test

--- belajar-testing/src/test/java/belajar/testing/CalculatorTest.java
+++ belajar-testing/src/test/java/belajar/testing/CalculatorTest.java
@@ -1,24 +1,28 @@
 package belajar.testing;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
+    @DisplayName("Test calculator add succes")
     @Test
     public void TestAddSuccess(){
         var result = calculator.add(1,1);
         Assertions.assertEquals(2, result);
     }
 
+    @DisplayName("Test calculator divide succes")
     @Test
     public void TestDivideSuccess(){
         var result = calculator.divide(10,5);
         Assertions.assertEquals(2, result);
     }
 
+    @DisplayName("Test calculator divide failed")
     @Test
     public void TestDivideFailed(){
         Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(0,5));
