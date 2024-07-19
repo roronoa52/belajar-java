fb084f3ab95ebc86f06d3ae56327fcd9d6c54ea5
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-20 00:10:39 +0700

    learn preprae statement

---
+++ belajar-database/src/test/java/belajar/database/java/PrepareStatementTest.java
@@ -0,0 +1,35 @@
+package belajar.database.java;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+public class PrepareStatementTest {
+
+    @Test
+    void TestPrepareStatement() throws SQLException {
+
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+
+        var admin = "admin1";
+        var password = "12345678";
+
+        String sql = " SELECT * FROM admins WHERE username = ? AND password = ? ";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, admin);
+        preparedStatement.setString(2, password);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if(resultSet.next()){
+
+            System.out.println("Berhasil login dengan username " + resultSet.getString("username"));
+
+        }else {
+            System.out.println("Gagal login");
+        }
+
+    }
+
+}
