83507fd8d2c3f65a2bc8cc38c58d0799b426b353
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-19 19:58:35 +0700

    Learn Collectors

---
+++ belajar-stream/src/test/java/belajar/stream/java/CollectorsTest.java
@@ -0,0 +1,34 @@
+package belajar.stream.java;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class CollectorsTest {
+
+    @Test
+    void testCreate() {
+        IntStream intStream = IntStream.range(1, 100);
+        intStream.forEach(System.out::println);
+
+        LongStream longStream = LongStream.of(1, 2, 3, 4);
+
+        DoubleStream doubleStream = DoubleStream.builder().add(0.1).add(0.2).build();
+    }
+
+    @Test
+    void testOperations() {
+        IntStream intStream = IntStream.range(1, 100);
+
+        OptionalDouble optionalDouble = intStream.average();
+        optionalDouble.ifPresent(System.out::println);
+
+        IntStream.range(0, 100)
+                .mapToObj(number -> "Number : " + number)
+                .forEach(System.out::println);
+    }
+
+}
