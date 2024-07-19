992f64a5da90376e43b22137b008bb07ca30c2d2
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-19 11:50:08 +0700

    Test yang berjalan secara paralel

--- belajar-testing/src/test/java/belajar/testing/SlowTest.java
+++ belajar-testing/src/test/java/belajar/testing/SlowTest.java
@@ -2,15 +2,30 @@ package belajar.testing;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class SlowTest {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
-    void testSlow() throws InterruptedException{
-        Thread.sleep(10_000);
+    void testSlow1() throws InterruptedException{
+        Thread.sleep(4_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow2() throws InterruptedException{
+        Thread.sleep(4_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow3() throws InterruptedException{
+        Thread.sleep(4_000);
     }
 
 }
